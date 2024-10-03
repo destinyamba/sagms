ec028fcc51fc6bf68b61a1ad223aa4804465e9ce
destinyamba <destiny.amba@gmail.com>
2024-10-04 00:27:19 +0100

    feat: add basic user functionality

--- app.py
+++ app.py
@@ -97,6 +97,31 @@ def generate_artworks_dummy_data():
     return artwork_dict
 
 
+def generate_users_dummy_data():
+
+    user_dict = {}
+
+    roles = ["VISITOR", "CURATOR", "ADMIN"]
+
+    for i in range(10):
+        id = str(uuid.uuid4())
+        username = "user" + str(i)
+        email = f"{username}@{username}.com"
+        password = "password"
+        role = roles[random.randint(0, len(roles) - 1)]
+        created_at = "2023-12-01T00:00:00"
+
+        user_dict[id] = {
+            "username": username,
+            "email": email,
+            "password": password,
+            "role": role,
+            "created_at": created_at,
+            "updated_at": created_at,
+        }
+    return user_dict
+
+
 @app.route("/", methods=["GET"])
 def index():
     return make_response("<h1>Hello, World! Destiny's making a flask app.</h1>", 200)
@@ -192,6 +217,79 @@ def delete_artwork(artist_id, artwork_id):
         return make_response(jsonify({}), 200)
 
 
+# User endpoints
+
+# Only and ADMIN can create users.
+# All users including artists can comment and review artworks and exhibitions.
+
+
+@app.route("/api/v1.0/users", methods=["GET"])
+def get_users():
+    page_num, page_size = 1, 10
+    if request.args.get("pn"):
+        page_num = int(request.args.get("pn"))
+    if request.args.get("ps"):
+        page_size = int(request.args.get("ps"))
+    page_start = page_size * (page_num - 1)
+    users_list = [{k: v} for k, v in users.items()]
+    return make_response(jsonify(users_list[page_start : page_start + page_size]), 200)
+
+
+@app.route("/api/v1.0/users/<string:user_id>", methods=["GET"])
+def get_user(user_id):
+    if user_id not in users:
+        return make_response(jsonify({"error": "User not found"}), 404)
+    return make_response(jsonify(users[user_id]), 200)
+
+
+@app.route("/api/v1.0/users/<string:user_id>", methods=["POST"])
+def create_user(user_id):
+    if users[user_id]["role"] != "ADMIN":
+        return make_response(jsonify({"error": "Unauthorized to update user"}), 401)
+    else:
+        data = request.get_json()
+        next_id = str(uuid.uuid4())
+        new_user = {
+            "username": data.get("username", "username"),
+            "email": data.get("email", "email"),
+            "password": data.get("password", "password"),
+            "role": data.get("role", "role"),
+            "created_at": "2023-12-01T00:00:00",
+            "updated_at": "2023-12-01T00:00:00",
+        }
+        users[next_id] = new_user
+        return make_response(jsonify({next_id: new_user}), 201)
+
+
+@app.route("/api/v1.0/users/<string:user_id>", methods=["PUT"])
+def update_user(user_id):
+    data = request.get_json()
+    if user_id not in users:
+        return make_response(jsonify({"error": "User not found"}), 404)
+    elif users[user_id]["role"] != "ADMIN":
+        return make_response(jsonify({"error": "Unauthorized to update user"}), 401)
+    else:
+        for field in data:
+            if field in ["username", "email", "password", "role"]:
+                users[user_id][field] = data.get(field, users[user_id][field])
+            else:
+                return make_response(jsonify({"error": "Missing required fields"}), 400)
+
+    return make_response(jsonify({user_id: users[user_id]}), 200)
+
+
+@app.route("/api/v1.0/users/<string:user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    if user_id not in users:
+        return make_response(jsonify({"error": "User not found"}), 404)
+    elif users[user_id]["role"] != "ADMIN":
+        return make_response(jsonify({"error": "Unauthorized to delete user"}), 401)
+    else:
+        del users[user_id]
+        return make_response(jsonify({}), 200)
+
+
 if __name__ == "__main__":
     artworks = generate_artworks_dummy_data()
+    users = generate_users_dummy_data()
     app.run(debug=True)
